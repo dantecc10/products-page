a21d2383386d3a078ad396e0a0751df2956d0cc5
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-12 13:06:47 -0600

    barcode

--- php scripts/barcode-test.php
+++ php scripts/barcode-test.php
@@ -4,7 +4,17 @@ require '../vendor/autoload.php'; // Carga la biblioteca a través de Composer
 use Picqer\Barcode\BarcodeGeneratorPNG;
 
 $generator = new BarcodeGeneratorPNG();
-$barcode = $generator->getBarcode('4008789701428', $generator::TYPE_CODE_128);
 
-echo '<img src="data:image/png;base64,' . base64_encode($barcode) . '">';
-?>
+// Variables dinámicas
+# $barcode = $generator->getBarcode('4008789701428', $generator::TYPE_CODE_128); # Constante de debug
+
+function bar_code($generator, $dynamicBarCode)
+{
+    $barcode = $generator->getBarcode($dynamicBarCode, $generator::TYPE_CODE_128);
+    #echo '<img class="barcode-img" width="100" height="80" src="data:image/png;base64,' . base64_encode($barcode) . '">';
+    return ('data:image/png;base64,' . base64_encode($barcode));
+}
+
+#echo '<img class="barcode-img" width="100" height="80" src="data:image/png;base64,' . base64_encode($barcode) . '">';
+
+echo ('<img src="'.bar_code($generator, "7501000278404").'" alt="Auto Barcode" class="barcode-img">');
