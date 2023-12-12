c23555e8c45a00c02dcea6563ec0bf27b2803b70
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-11 18:58:54 -0600

    Translated the error message from English to Spanish. (generated by blackbox.ai )

--- php scripts/connection.php
+++ php scripts/connection.php
@@ -3,8 +3,7 @@ include "credentials.php";
 $data = generatePasskey('sql');
 $connection = new mysqli("localhost", $data[0], $data[1], $data[2]);
 if ($connection->connect_error) {
-    die("
-    Connection failed: " . $connection->connect_error);
+    die("La conexión a la base de datos falló: " . $connection->connect_error);
 } else {
-    echo("Conexión establecida.");
+    echo ("Conexión establecida");
 }
