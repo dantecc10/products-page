5ff25137f18bf30ef9c4dafe97b6582248d578fd
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-12 01:55:39 -0600

    prueba playmobil

--- php scripts/barcode-test.php
+++ php scripts/barcode-test.php
@@ -4,7 +4,7 @@ require '../vendor/autoload.php'; // Carga la biblioteca a través de Composer
 use Picqer\Barcode\BarcodeGeneratorPNG;
 
 $generator = new BarcodeGeneratorPNG();
-$barcode = $generator->getBarcode('0123456789', $generator::TYPE_CODE_128);
+$barcode = $generator->getBarcode('4008789701428', $generator::TYPE_CODE_128);
 
 echo '<img src="data:image/png;base64,' . base64_encode($barcode) . '">';
 ?>
