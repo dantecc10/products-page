94e81e34b6dc7259cece5849151f442727c81edc
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-12 13:20:21 -0600

    barcode-generator.php

--- php scripts/barcode-generator.php
+++ php scripts/barcode-generator.php
@@ -2,7 +2,6 @@
 require '../vendor/autoload.php'; // Carga la biblioteca a través de Composer
 
 use Picqer\Barcode\BarcodeGeneratorPNG;
-
 $generator = new BarcodeGeneratorPNG();
 
 // Variables dinámicas
@@ -13,11 +12,9 @@ function bar_code_img($generator, $dynamicBarCode)
     $barcode = $generator->getBarcode($dynamicBarCode, $generator::TYPE_CODE_128);
     #echo '<img class="barcode-img" width="100" height="80" src="data:image/png;base64,' . base64_encode($barcode) . '">';
     $img = ('<img src="data:image/png;base64,' . base64_encode($barcode) . '" alt="Código de barras: ' . $dynamicBarCode . '" class="barcode-img">');
-    return $img;
+    return $img; # Devuelve imagen con clase para adaptar con CSS
 }
-
-#echo '<img class="barcode-img" width="100" height="80" src="data:image/png;base64,' . base64_encode($barcode) . '">';
-
-#echo ('<img src="' . bar_code_img($generator, "7501000278404") . '" alt="Auto Barcode" class="barcode-img">');
+/* Ejemplo de invocación:
 $number = "7501000278404";
-echo bar_code_img($generator, $number);
+echo bar_code_img($generator, $number); // Imprime HTML <img>
+*/
