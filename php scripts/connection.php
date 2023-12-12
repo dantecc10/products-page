b740c5f1e8a081c248b63b6f67d3f0b363894e00
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-11 18:56:11 -0600

    Changed the way the database connection is established. - Previously, the connection details were included from an external file. - Now, the connection details are generated using the `generatePasskey` function and stored in the `credentials.php` file. (generated by blackbox.ai )

--- php scripts/connection.php
+++ php scripts/connection.php
@@ -1,2 +1,11 @@
 <?php
-include "https://castelancarpinteyro.com/php scripts/dynamicSecrets.php";
+include "credentials.php";
+$data = generatePasskey('sql');
+$connection = new mysqli("localhost", $data[0], $data[1], $data[2]);
+if ($connection->connect_error) {
+    die("
+    Connection failed: " . $connection->connect_error);
+} else {
+    $query = "
+        ";
+}
