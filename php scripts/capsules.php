f537c6180d11b5f5f722fb83cfacfb4b96b29706
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-12 20:37:32 -0600

    creación de cápsulas de DOM

---
+++ php scripts/capsules.php
@@ -0,0 +1,59 @@
+            <tr id="article-row-1" class="align-middle articles-row">
+                <td class="article-icon"><a href="detalle.html"><img class="mini-image" src="https://media.playmobil.com/i/playmobil/71162_product_detail/Polic%C3%ADa%20con%20Perro?locale=es-MX,es,en,*&amp;$pdp_product_main_xs$&amp;fmt=auto&amp;strip=true&amp;qlt=80&amp;fmt.jpeg.chroma=1,1,1&amp;unsharp=0,1,1,7&amp;fmt.jpeg.interlaced=true"></a></td>
+                <td><a href="detalle.html"><span style="color: rgb(133, 135, 150);">Producto 2 de prueba para tablita</span><br></a></td>
+                <td><a href="detalle.html"><span style="color: rgb(133, 135, 150);">Playmobil</span><br></a></td>
+                <td><a href="detalle.html"><span style="color: rgb(133, 135, 150);">$279</span><br></a></td>
+                <td>
+                    <div class="col"><span id="quant-1"><a href="detalle.html">3</a></span></div>
+                </td>
+                <td><a href="detalle.html"><span style="color: rgb(133, 135, 150);">70142</span><br></a></td>
+                <td>
+                    <div class="col d-flex align-middle">
+                        <div class="row" style="margin: 0px !important;">
+                            <div class="col">
+                                <div class="row">
+                                    <div class="col"><a href="detalle.html"><span style="font-size: 100% !important;">4008789701428</span></a></div>
+                                </div>
+                                <div class="row">
+                                    <div class="col" style="padding: 3px !important;"><a href="detalle.html"><img class="barcode-img" src="assets/img/test-barcode.png"></a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><br>
+                </td>
+            </tr>
+
+
+
+            <?php
+
+            $capsule['A'][0] = ['<div class="table-responsive table mt-2 text-center" id="dataTable" role="grid" aria-describedby="dataTable_info">
+                                    <table class="table my-0 cell-text" id="dataTable">
+                                        <thead>
+                                            <tr>
+                                                <th class="align-middle">Detalle</th>
+                                                <th>Nombre</th>
+                                                <th>Marca</th>
+                                                <th>Precio</th>
+                                                <th>Piezas</th>
+                                                <th>Modelo</th>
+                                                <th>Barras</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody id="table-toys" class="articles-table">'];
+
+            $capsule['A'][1] = ['        </tbody>
+                                         <tfoot>
+                                             <tr>
+                                                 <td class="align-middle"><strong>Detalle</strong></td>
+                                                 <td><strong>Nombre</strong></td>
+                                                 <td><strong>Marca</strong></td>
+                                                 <td><strong>Precio</strong></td>
+                                                 <td><strong>Piezas</strong></td>
+                                                 <td><strong>Modelo</strong></td>
+                                                 <td><strong>Barras</strong></td>
+                                             </tr>
+                                         </tfoot>
+                                     </table>
+                                 </div>'];
+            ?>
