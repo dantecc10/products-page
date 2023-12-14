9ab1dd9237e684422fa77111e951396f7ae47f86
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-13 19:54:13 -0600

    Ajustes en script directo de HTML

--- articles.php
+++ articles.php
@@ -269,7 +269,32 @@ if (!isset($_SESSION['id'])) {
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/theme.js"></script>
-    <script src="assets/js/table-builder.js"></script>
+    <!--<script src="assets/js/table-builder.js"></script>-->
+    <script>
+        function build_table(table) {
+            // Obtener los valores de los campos de entrada
+            let table = "toys";
+
+            let objective = document.getElementById("table-toys");
+
+            // Crear objeto XMLHttpRequest
+            let xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    // Procesar la respuesta del servidor
+                    if (this.responseText != null) {
+                        // La construcción de la tabla no es nula y procede
+                        objective.innerHTML = this.responseText;
+                    } else {
+                        // La respuesta es nula, interpretar como que no se encontraron datos y avisar vacío
+                    }
+                }
+            };
+            xhr.open("GET", "../../php scripts/build-table.php", true);
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhr.send("table=" + table);
+        }
+    </script>
     <div onload="javascript:build_table('juguetes');" style="display: none"></div>
 </body>
 
