d3ec3bfd22265a9ae901543a2d3705691a0c41c4
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-15 15:18:24 -0600

    carga para validación inversa

--- php scripts/functions.php
+++ php scripts/functions.php
@@ -1,8 +1,18 @@
 <?php
+function contains_string($main_string, $substring)
+{
+    // strpos devuelve la posición donde se encuentra la subcadena
+    // Si no se encuentra, devuelve false
+    return strpos($main_string, $substring) !== false;
+}
 function split_urls($urls)
 {
-    $img_urls = array();
-    $img_urls = explode(", ", $urls);
+    if (contains_string($urls, ", ")) {
+        $img_urls = array();
+        $img_urls = explode(", ", $urls);
+    } else {
+        $img_urls = $urls;
+    }
     return $img_urls; // Usar como $data = split_urls($cadena_con_urls); (Validar con length)
 }
 function splitter($urls, $divisor)
@@ -32,27 +42,52 @@ function build_table_dom($table, $data, $fields)
 
 function build_detail_carousel($imgs)
 {
+    $carousel = '';
     $n = sizeof($imgs);
 
-    $carousel_elements = "";
-    $carousel_indexes = "";
+    $carousel_capusule1 = '<div id="carousel-1" class="carousel slide" data-bs-ride="carousel">';
+    $carousel_capusule2 = '</div>';
+
+    $carousel_elements = '<div class="carousel-inner">';
+    $carousel_indexes = '<ol class="carousel-indicators">';
+
+    $carousel_buttons = '<div>
+    <a class="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+      <span class="visually-hidden">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next">
+      <span class="carousel-control-next-icon"></span>
+      <span class="visually-hidden">Next</span>
+    </a>
+  </div>';
 
     for ($i = 0; $i < $n; $i++) {
         $element = '<div class="carousel-item">';
         $index = '<li data-bs-target="#carousel-1" data-bs-slide-to="';
-        
+
         if ($i == 0) {
             // Añadir clases active al primer elemento e índice
             $element = str_replace('">', 'active">', $element);
             $index = str_replace('<li ', '<li class="active" ', $index);
-        } else {
         }
 
-        $element .= ($i+1);
+        $element .= ('<img class="w-100 d-block" src="' . ($imgs[$i]) . '" alt="Imagen ' . ($i + 1) . '" />');
         $index .= $i;
-
 
         $element .= '"</div>';
         $index .= '"></li>';
+
+        $carousel_elements .= $element;
+        $carousel_indexes .= $index;
     }
+
+    $carousel_elements .= '</div>';
+    $carousel_indexes .= '</ol>';
+    $carousel .= ($carousel_capusule1 . $carousel_elements . $carousel_buttons . $carousel_indexes . $carousel_capusule2);
+    return $carousel;
+    /*
+        Invocación:
+        build_detail_carousel($split_urls($data[$10])) // $data[10] es imgs de SQL
+    */
 }
