35d908bdba10f045b6e9724ae7b12d77de8a7e49
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-18 00:55:11 -0600

    update function

--- assets/js/table-filter.js
+++ assets/js/table-filter.js
@@ -1,27 +1,26 @@
 function table_filter() {
-  var search_text = document.getElementById("filtering-input").value;
-  // Obtener los valores de los campos de entrada
-  //let table = "juguetes";
-  if (search_text != "" && search_text != null && search_text != " ") {
-    var objective = document.getElementById("table-toys");
-
-    // Crear objeto XMLHttpRequest
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        // Procesar la respuesta del servidor
-        if (this.responseText != null) {
-          // La construcción de la tabla no es nula y procede
-          objective.innerHTML = this.responseText;
-        } else {
-          // La respuesta es nula, interpretar como que no se encontraron datos y avisar vacío
-        }
-      }
-    };
-    xhr.open("GET", "php scripts/build-table.php", true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send("filter=" + search_text);
-  }
+    search_text = document.getElementById("filtering-input").value;
+    // Obtener los valores de los campos de entrada
+    //let table = "juguetes";
+    if (search_text != "" && search_text != null && search_text != " ") {
+        objective = document.getElementById("table-toys");
+        // Crear objeto XMLHttpRequest
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // Procesar la respuesta del servidor
+                if (this.responseText != null) {
+                    // La construcción de la tabla no es nula y procede
+                    objective.innerHTML = this.responseText;
+                } else {
+                    // La respuesta es nula, interpretar como que no se encontraron datos y avisar vacío
+                }
+            }
+        };
+        xhr.open("GET", "php scripts/build-table.php", true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send("filter=" + search_text);
+    }
 }
 
 /*
