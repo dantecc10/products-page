ebbd06f5964ba51d237c3ed6cbd459e562fd631d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-19 16:46:00 -0600

    faltó el subdomino en link

--- php scripts/qr-generator.php
+++ php scripts/qr-generator.php
@@ -3,7 +3,7 @@ require '../vendor/autoload.php';
 
 use chillerlan\QRCode\QRCode;
 
-$data = 'castelancarpinteyro.com/details.php?product=15';
+$data = 'comercial.castelancarpinteyro.com/details.php?product=15';
 
 // quick and simple:
 echo '<img src="' . (new QRCode)->render($data) . '" alt="QR Code" />';
