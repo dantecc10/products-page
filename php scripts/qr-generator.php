facdf48d333a894fe8c536f2d3d0609e32943343
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-19 16:40:48 -0600

    docs example

---
+++ php scripts/qr-generator.php
@@ -0,0 +1,9 @@
+<?php
+require '../vendor/autoload.php';
+
+use chillerlan\QRCode\QRCode;
+
+$data = 'otpauth://totp/test?secret=B3JX4VCVJDVNXNZ5&issuer=chillerlan.net';
+
+// quick and simple:
+echo '<img src="' . (new QRCode)->render($data) . '" alt="QR Code" />';
