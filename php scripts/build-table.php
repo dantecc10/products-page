21e2e2451cf6c45df0fd1495dca52d49189ac3be
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-23 15:43:41 -0600

    changes

--- php scripts/build-table.php
+++ php scripts/build-table.php
@@ -13,19 +13,8 @@ switch ($tabla) {
     case 'juguetes':
         $campos = array();
         $campos = [
-            "id_toy",
-            "name_toy",
-            "description_toy",
-            "model_toy",
-            "line_toy",
-            "bars_toy",
-            "brand_toy",
-            "pieces_toy",
-            "quantity_toy",
-            "price_toy",
-            "imgs_toy",
-            "quant_imgs_toy",
-        ];
+            "id_toy", "name_toy", "description_toy", "model_toy", "line_toy", "bars_toy", "brand_toy", "pieces_toy", "quantity_toy",
+            "price_toy", "imgs_toy", "quant_imgs_toy"];
         break;
 
     default:
