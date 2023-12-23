039ec71342d95e191e8bff9d87a69c7c93a44771
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-23 15:42:36 -0600

    modificaciones

--- php scripts/sql-fetcher.php
+++ php scripts/sql-fetcher.php
@@ -2,20 +2,8 @@
 /*
 $tabla = "juguetes";
 $campos = array();
-$campos = [
-    "id_toy",
-    "name_toy",
-    "description_toy",
-    "model_toy",
-    "bars_toy",
-    "line_toy",
-    "brand_toy",
-    "pieces_toy",
-    "quantity_toy",
-    "price_toy",
-    "imgs_toy",
-    "quant_imgs_toy",
-];
+$campos = ["id_toy", "name_toy", "description_toy", "model_toy", "bars_toy", "line_toy", "brand_toy", "pieces_toy", "quantity_toy",
+    "price_toy", "imgs_toy", "quant_imgs_toy"];
 */
 
 function fetch_fields($table, $fields, $id, $custom_query)
