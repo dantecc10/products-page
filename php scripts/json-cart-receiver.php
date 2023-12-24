4bfc38ee5c34091bc35104969d0a0ab76b485375
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-23 20:11:25 -0600

    test boolean return

--- php scripts/json-cart-receiver.php
+++ php scripts/json-cart-receiver.php
@@ -1,9 +1,15 @@
 <?php
 // Recibir el JSON enviado desde JavaScript
 $json = file_get_contents('php://input');
+if (file_get_contents('php://input')) {
+    echo ("Se recibe algo");
+} else {
+    echo ("No se recibe nada");
+}
 
 // Decodificar el JSON a un array o objeto PHP
 $json_cart = json_decode($json, true); // true para obtener un array asociativo
 
 // Puedes realizar acciones con los datos recibidos, por ejemplo, imprimirlos
-print_r($json_cart);
+print_r($json_cart); // Debug line
+//echo true;
