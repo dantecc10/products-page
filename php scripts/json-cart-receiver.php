9e39c0a31935bf9eaf26d9ca7f16f1dbbda096f9
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-23 20:24:10 -0600

    echo de category con json php

--- php scripts/json-cart-receiver.php
+++ php scripts/json-cart-receiver.php
@@ -1,15 +1,14 @@
 <?php
 // Recibir el JSON enviado desde JavaScript
-$json = file_get_contents('php://input');
 if (file_get_contents('php://input')) {
+    $json = file_get_contents('php://input');
     echo ("Se recibe algo");
+    // Decodificar el JSON a un array o objeto PHP
+    $json_cart = json_decode($json, true); // true para obtener un array asociativo
+    // Puedes realizar acciones con los datos recibidos, por ejemplo, imprimirlos
+    //print_r($json_cart); // Debug line
+    //echo true;
+    echo ($json_cart['Products'][0]['category']);
 } else {
     echo ("No se recibe nada");
 }
-
-// Decodificar el JSON a un array o objeto PHP
-$json_cart = json_decode($json, true); // true para obtener un array asociativo
-
-// Puedes realizar acciones con los datos recibidos, por ejemplo, imprimirlos
-print_r($json_cart); // Debug line
-//echo true;
