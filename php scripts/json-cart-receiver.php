bc3c78724ae3ace581e90de4c42ab07362b5def0
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-23 20:24:30 -0600

    echo true

--- php scripts/json-cart-receiver.php
+++ php scripts/json-cart-receiver.php
@@ -7,7 +7,7 @@ if (file_get_contents('php://input')) {
     $json_cart = json_decode($json, true); // true para obtener un array asociativo
     // Puedes realizar acciones con los datos recibidos, por ejemplo, imprimirlos
     //print_r($json_cart); // Debug line
-    //echo true;
+    echo true;
     echo ($json_cart['Products'][0]['category']);
 } else {
     echo ("No se recibe nada");
