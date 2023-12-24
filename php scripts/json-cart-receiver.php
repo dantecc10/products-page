2ac7d315906ab5ae992c297070e8f30634f64766
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-23 23:17:30 -0600

    echo encode $_session['cart']

--- php scripts/json-cart-receiver.php
+++ php scripts/json-cart-receiver.php
@@ -3,16 +3,22 @@ session_start();
 // Recibir el JSON enviado desde JavaScript
 if (file_get_contents('php://input')) {
     $json = file_get_contents('php://input');
-    echo ("Se recibe algo");
+    //echo ("Se recibe algo");
     // Decodificar el JSON a un array o objeto PHP
     $json_cart = json_decode($json, true); // true para obtener un array asociativo
     // Puedes realizar acciones con los datos recibidos, por ejemplo, imprimirlos
-    //print_r($json_cart); // Debug line
     $_SESSION['cart'] = $json_cart;
-    echo true;
+    echo ("true");
+    /* Líneas para entender propiedades
     echo ($json_cart['Products'][0]['category']);
     echo($_SESSION['cart']['Products'][0]['category']);
+    */
 } else {
     echo ("No se recibe nada");
     //$json_cart = json_encode();
+    $n = sizeof($_SESSION['cart']['Products']);
+    echo (json_encode($_SESSION['cart']));
+    //for ($i=0; $i < $n; $i++) { 
+    //    echo();
+    //}
 }
