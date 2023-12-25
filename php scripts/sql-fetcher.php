fb24bd25f469f3893734e54f476fccc4abcd4956
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-25 14:05:59 -0600

    llaves mal acomodadas

--- php scripts/sql-fetcher.php
+++ php scripts/sql-fetcher.php
@@ -27,24 +27,25 @@ function fetch_fields($table, $fields, $id, $custom_query)
     // Comprobar si las filas son mayores que 0
     $result = $connection->query($query);
     // Verificar si se encontró un usuario válido
-    if ((stripos($query, "UPDATE") == false)){
-        if($result->num_rows > 0) {
-        $i = 0;
-        // Hacer fetch a los datos
-        while ($row = $result->fetch_array()) {
-            // Procesar cada registro obtenido
-            for ($j = 0; $j < $n; $j++) {
-                if ($id == "" or $id == null) {
-                    // Procesar cada columna de cada registro 
-                    $data[$i][$j] = $row[$fields[$j]];
-                } else {
-                    // Procesar cada columna de cada registro 
-                    $data[$j] = $row[$fields[$j]];
+    if ((stripos($query, "UPDATE") == false)) {
+        if ($result->num_rows > 0) {
+            $i = 0;
+            // Hacer fetch a los datos
+            while ($row = $result->fetch_array()) {
+                // Procesar cada registro obtenido
+                for ($j = 0; $j < $n; $j++) {
+                    if ($id == "" or $id == null) {
+                        // Procesar cada columna de cada registro 
+                        $data[$i][$j] = $row[$fields[$j]];
+                    } else {
+                        // Procesar cada columna de cada registro 
+                        $data[$j] = $row[$fields[$j]];
+                    }
                 }
+                $i++;
             }
-            $i++;
+            return $data;
         }
-        return $data;
     } else {
         // No hay registros en la tabla, devolver null
         return null;
@@ -52,22 +53,7 @@ function fetch_fields($table, $fields, $id, $custom_query)
 }
 
 //Debug zone
-/*
-$tabla = "juguetes";
-    $campos = array();
-    $campos = [
-        "id_toy",
-        "name_toy",
-        "description_toy",
-        "model_toy",
-        "line_toy",
-        "bars_toy",
-        "brand_toy",
-        "pieces_toy",
-        "quantity_toy",
-        "price_toy",
-        "imgs_toy",
-        "quant_imgs_toy",
-    ];
-echo (fetch_fields($tabla, $campos, null, null))[23][1]; # Línea de prueba (comentar)
-*/
+//$tabla = "juguetes";
+//$campos = array();
+//$campos = ["id_toy", "name_toy", "description_toy", "model_toy", "line_toy", "bars_toy", "brand_toy", "pieces_toy", "quantity_toy", "price_toy", "imgs_toy", "quant_imgs_toy"];
+//echo (fetch_fields($tabla, $campos, null, null))[23][1]; # Línea de prueba (comentar)
