347ad2bd43d35329c2a817101a0b2dd35f72aec8
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-25 13:59:19 -0600

    revisar que la consulta no sea tipo update

--- php scripts/sql-fetcher.php
+++ php scripts/sql-fetcher.php
@@ -27,7 +27,7 @@ function fetch_fields($table, $fields, $id, $custom_query)
     // Comprobar si las filas son mayores que 0
     $result = $connection->query($query);
     // Verificar si se encontró un usuario válido
-    if ($result->num_rows > 0) {
+    if ((strpos($query, "UPDATE") == false) && ($result->num_rows > 0)) {
         $i = 0;
         // Hacer fetch a los datos
         while ($row = $result->fetch_array()) {
