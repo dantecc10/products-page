1fe9c66469c9841af96daa1f14727cc3ee47f96a
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-26 00:17:11 -0600

    veamos qué sucede

--- php scripts/ticket-generator.php
+++ php scripts/ticket-generator.php
@@ -83,10 +83,14 @@ $html = ('
 <body>
     <div class="ticket">
         <div class="header">
-            <div style="width: 50%; margin-left: 12.5%;">
-                <img src="../assets/img/branding/logo.jpeg" alt="Logo" width="20mm">
+            <div class="row">
+                <div id="logo-container" style="width: 50%; margin-left: 12.5%;" class="col">
+                    <img src="../assets/img/branding/logo.jpeg" alt="Logo" width="20mm">
+                </div>
+                <div class="col" style="width: 50%;">
+                    <h1 style="font-size: 7mm">Comercial: Castelán Carpinteyro</h1>
+                </div>
             </div>
-            <h1 style="font-size: 7mm">Comercial: Castelán Carpinteyro</h1>
             <h2 style="font-size: 5mm">Ticket de Venta</h2>
             <p>Domicilio de Ejemplo No. 1, en la Calle Algo Así, Barrio Inexistente.</p>
             <p>C.P.: 00000. Ciudad Actual, Estado Impreso.</p>
