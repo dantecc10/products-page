5e1dabc9e6d6bedbf3066f1af0966b5353cc1d9b
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-25 19:56:03 -0600

    prueba de pt

--- php scripts/ticket-generator.php
+++ php scripts/ticket-generator.php
@@ -45,75 +45,52 @@ $pdf->AddPage();
 //    
 //';
 
-$html = '
-    <!DOCTYPE html>
-    <html lang="es-MX">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-        <title>Document</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
-        <link rel="stylesheet" href="../assets/css/Bootstrap-Callout-Info.css">
-        <link rel="stylesheet" href="../assets/css/extra.css">
-    </head>
-    <body>
-        <style>
-            th.w-5, td.w-5 {
-                width: 5%; 
-            }
-            th.w-10, td.w-10 {
-                width: 10%;          
-            }
-            th.w-20, td.w-20 {
-                width: 20%;          
-            }
-            th.w-55, td.w-55 {
-                width: 55%;          
-            }
-        </style>
-
-        <div class="container">
-            <div class="row">
-                <div class="col col-6 d-flex justify-content-center" style="width: 50%;">
-                    <img src="../assets/img/branding/logo.jpeg" width="auto">
-                </div>
-                <div class="col col-6 d-flex justify-content-center" style="width: 50%;">
-                    Divisor
-                </div>
-            </div>
+$html = '<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ticket de Venta</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        .ticket {
+            border: 1px solid #ccc;
+            padding: 10px;
+            width: 300px;
+        }
+        .header {
+            text-align: center;
+            margin-bottom: 10px;
+        }
+        .items {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+        .item {
+            margin-bottom: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="ticket">
+        <div class="header">
+            <h2>Ticket de Venta</h2>
+            <p>Fecha: 24 de diciembre de 2023</p>
         </div>
-
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th class="w-5">ID</th>
-                    <th class="w-55">DESC</th>
-                    <th class="w-10">PRECIO</th>
-                    <th class="w-10">UDS.</th>
-                    <th class="w-20">SUBTOTAL</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td class="w-5">13</td>
-                    <td class="w-55">AVIÓN PASAJEROS PLAYMOBIL</td>
-                    <td class="w-10">$671</td>
-                    <td class="w-10">1</td>
-                    <td class="w-20">$671</td>
-                </tr>
-            </tbody
-            <tfooter>
-                <td>ID</td>
-                <td>DESC</td>
-                <td>PRECIO</td>
-                <td>UDS.</td>
-                <td>SUBTOTAL</td>
-            </tfooter>
-        </table>
-    </body>
-    </html>
+        <ul class="items">
+            <li class="item">Producto 1: $10</li>
+            <li class="item">Producto 2: $20</li>
+            <!-- Agrega más productos según sea necesario -->
+        </ul>
+        <hr>
+        <p>Total: $30</p>
+        <p>Gracias por su compra</p>
+    </div>
+</body>
+</html>
 ';
 
 // Agregar el HTML al PDF
