cfc159bf421922c0b16f36dd4743e2ab4a42c204
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-26 12:10:54 -0600

    intval

--- php scripts/sell-cart.php
+++ php scripts/sell-cart.php
@@ -24,8 +24,8 @@ if (isset($_SESSION['cart'])) {
         $type = "sale";
         $user = 1;
     }/*else{}*/
-    $total_transaction = floatval($total_transaction);
-    echo ($total_transaction);
+    $total_transaction = intval($total_transaction);
+    //echo ($total_transaction);
     $data = array(6);
     $data = [$channel, $articles_transaction, $categories, $total_transaction, $type, $user];
     if (sql_transaction_insert($data, "transacciones") !== false) {
