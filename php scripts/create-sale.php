9921b44bc3fd7ecd2dedea399a8a0f202d9869bd
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-26 21:55:03 -0600

    creando lógica de pagos

---
+++ php scripts/create-sale.php
@@ -0,0 +1,7 @@
+<?php
+    if($_POST['payment-method'] !== '3'){
+        // El método de pago seleccionado no es PayPal
+        
+    }else{
+        //El método de pago seleccionado fue PayPal, iniciar configuración
+    }
