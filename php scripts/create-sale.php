76cb752cd3c1d1cb1f92ba5eff62f4d65b35fe36
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-26 23:40:37 -0600

    setting some data form post

--- php scripts/create-sale.php
+++ php scripts/create-sale.php
@@ -1,8 +1,13 @@
 <?php
-    if($_POST['payment-method'] !== '3'){
-        // El método de pago seleccionado no es PayPal
-
-    }else{
-        //El método de pago seleccionado fue PayPal, iniciar configuración
+session_start();
+if ($_POST['payment-method'] !== '3') {
+    // El método de pago seleccionado no es PayPal
+    $_SESSION['sale']['payment'] = $_POST['payment-method'];
+    $_SESSION['sale']['payment']['digital-ticket'] = (isset($_POST['digital-ticket-checkbox'])) ? true : false;
+    $_SESSION['sale']['payment']['printed-ticket'] = (isset($_POST['printed-ticket-checkbox'])) ? true : false;
+    if ($_SESSION['sale']['payment']['digital-ticket']) {
+        $_SESSION['sale']['payment']['customer-email'] = $_POST['customer-email-input'];
     }
-    ?>
+} else {
+    //El método de pago seleccionado fue PayPal, iniciar configuración
+}
