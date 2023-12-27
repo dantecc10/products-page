9f4c5b876a6e63cf9bbd65715facfe542a8d9e80
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-27 00:00:40 -0600

    ping

--- php scripts/create-sale.php
+++ php scripts/create-sale.php
@@ -9,9 +9,13 @@ if ($_POST['payment-method'] !== '3') {
     //$_SESSION['sale']['payment']['digital-ticket'] = (isset($_POST['digital-ticket-checkbox'])) ? true : false;
     if (isset($_POST['digital-ticket-checkbox'])) {
         $_SESSION['sale']['payment']['digital-ticket'] = true;
+    }else{
+        $_SESSION['sale']['payment']['digital-ticket'] = false;
     }
     if (isset($_POST['printed-ticket-checkbox'])) {
         $_SESSION['sale']['payment']['printed-ticket'] = true;
+    }else{
+        $_SESSION['sale']['payment']['printed-ticket'] = false;
     }
     if ($_SESSION['sale']['payment']['digital-ticket']) {
         $_SESSION['sale']['payment']['customer-email'] = $_POST['customer-email-input'];
