07b5974e49763f4e7c5323d36e576026f9ebf666
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-01-02 03:33:26 -0600

    ..

--- php scripts/flag_replacer.php
+++ php scripts/flag_replacer.php
@@ -1,22 +1,27 @@
 <?php
-$txt = "Hola, mi nombre es FLAG, y vivo en FLAG. Tengo FLAG años y estudio en FLAG.";
-$datos = ["Dante", "Puebla", 18, "BUAP"];
-$indexes = [3, 2, 1, 0];
-$flag = "FLAG";
-function flag_replacer($text, $flag, $data_array, $indexes_array)
-{
-    $chars = strlen($flag);
-    $n = substr_count($text, $flag);
-    if ($n == sizeof($indexes_array)) {
-        // Las apariciones de la flag en la cadena son las mismas que la longitud del arreglo de índices
-        for ($i = 0; $i < $n; $i++) {
-            $position = strpos($text, $flag);
-            $text = substr_replace($text, $data_array[$indexes_array[$i]], $position, $chars);
+    function flag_replacer($text, $flag, $data_array, $indexes_array)
+    {
+        $chars = strlen($flag);
+        $n = substr_count($text, $flag);
+        if ($n == sizeof($indexes_array)) {
+            // Las apariciones de la flag en la cadena son las mismas que la longitud del arreglo de índices
+            for ($i = 0; $i < $n; $i++) {
+                $position = strpos($text, $flag);
+                $text = substr_replace($text, $data_array[$indexes_array[$i]], $position, $chars);
+            }
+            return $text;
+        } else {
+            return null;
         }
-        return $text;
-    } else {
-        return null;
     }
-}
 
-echo (flag_replacer($txt, $flag, $datos, $indexes));
+    // Datos de ejemplo
+    $txt = "Hola, mi nombre es FLAG, y vivo en FLAG. Tengo FLAG años y estudio en FLAG.";
+    $datos = ["Dante", "Puebla", 18, "BUAP"];
+    $indexes = [0, 1, 2, 3];
+    $flag = "FLAG";
+    
+    echo (flag_replacer($txt, $flag, $datos, $indexes));
+
+    //Salida: "Hola, mi nombre es Dante, y vivo en Puebla. Tengo 18 años y estudio en BUAP."
+?>
