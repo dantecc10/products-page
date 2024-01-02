f5001f49e24096c251f7d7203d341b9d56ed668e
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-01-02 11:09:56 -0600

    ..

--- php scripts/functions.php
+++ php scripts/functions.php
@@ -241,3 +241,19 @@ function import_env_configs()
     return $dotenv;
     //$dotenv->load();
 }
+
+function flag_replacer($text, $flag, $data_array, $indexes_array)
+{
+    $chars = strlen($flag);
+    $n = substr_count($text, $flag);
+    if ($n == sizeof($indexes_array)) {
+        // Las apariciones de la flag en la cadena son las mismas que la longitud del arreglo de índices
+        for ($i = 0; $i < $n; $i++) {
+            $position = strpos($text, $flag);
+            $text = substr_replace($text, $data_array[$indexes_array[$i]], $position, $chars);
+        }
+        return $text;
+    } else {
+        return null;
+    }
+}
