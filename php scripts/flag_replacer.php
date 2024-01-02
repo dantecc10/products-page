8f11a2fda19bf0152ed00175ec2fca6b669ab133
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-01-02 03:22:12 -0600

    debug flag replacer

---
+++ php scripts/flag_replacer.php
@@ -0,0 +1,21 @@
+<?php
+$txt = "Hola, mi nombre es FLAG, y vivo en FLAG. Tengo FLAG años y estudio en FLAG.";
+$datos = ["Dante", "Puebla", 18, "BUAP"];
+$indexes = [0, 1, 2, 3];
+$flag = "FLAG";
+function flag_replacer($text, $flag, $data_array, $indexes_array)
+{
+    $chars = strlen($flag);
+    $n = substr_count($text, $flag);
+    if (substr_count($text, $flag) == sizeof($indexes_array)) {
+        // Las apariciones de la flag en la cadena son las mismas que la longitud del arreglo de índices
+        for ($i = 0; $i < $n; $i++) {
+            $position = strpos($text, $flag);
+            $text = substr_replace($text, $data_array[$indexes_array[$i]], $position, $chars);
+        }
+    } else {
+        return null;
+    }
+}
+
+echo (flag_replacer($txt, $flag, $datos, $indexes));
