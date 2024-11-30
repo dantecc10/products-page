75a56ab2cf988fa8f4f1df8bed25b8c74dc3fabd
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-11-30 12:15:47 -0600

    semicolon

--- php-scripts/login.php
+++ php-scripts/login.php
@@ -11,7 +11,7 @@ if ($_POST['email'] == "demo_user@system.com") {
 $username = mysqli_real_escape_string($connection, $_POST['email']);
 $password = mysqli_real_escape_string($connection, $_POST['password']); //Recepción de variables que pasan por filtro anti explits SQL
 
-$sql = "SELECT * FROM `usuarios` WHERE (`email_user` = '$username') AND (`password_user` = '$password') AND (`status_user` = 1)";
+$sql = "SELECT * FROM `usuarios` WHERE (`email_user` = '$username') AND (`password_user` = '$password') AND (`status_user` = 1);";
 $resultado = $connection->query($sql);
 
 // Verificar si se encontró un usuario válido
