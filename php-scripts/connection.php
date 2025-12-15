09ef3cec1a5302fdcdd32bbbd910b3646d7da6ca
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2025-12-15 14:18:38 -0600

    includ_once

--- php-scripts/connection.php
+++ php-scripts/connection.php
@@ -1,5 +1,5 @@
 <?php
-include "credentials.php";
+include_once "credentials.php";
 $data = generatePasskey('sql');
 $connection = new mysqli("localhost", $data[0], $data[1], 'comercial');
 if ($connection->connect_error) {
